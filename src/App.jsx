b7c79019827b3ff4961d96cac9f148a2c4308295
Alisher <zumanovaliser814@gmail.com>
2022-10-18 21:05:01 +0600

    hm-7.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,10 @@
 import "./App.css";
-import Card from "./components/Card";
-const comment = [
-  {
-    id: "f1",
-    date: new Date(),
-    text: "I hope you enjoy learning React!",
-    author: {
-      name: "Hello Kitty",
-      avatarUrl: "http://placekitten.com/g/62/62",
-    },
-  },
-  {
-    id: "f2",
-    date: new Date(),
-    text: "I hope you enjoy learning React!",
-    author: {
-      name: "Hello Kitty",
-      avatarUrl: "http://placekitten.com/g/64/64",
-    },
-  },
-  {
-    id: "f3",
-    date: new Date(),
-    text: "I hope you enjoy learning React!",
-    author: {
-      name: "Hello Kitty",
-      avatarUrl: "http://placekitten.com/g/64/64",
-    },
-  },
-];
+import { Form } from "./Form";
 
 function App() {
   return (
     <div className="App">
-      {comment.map((item) => (
-        <Card {...item} key={item.id} />
-      ))}
+      <Form />
     </div>
   );
 }
